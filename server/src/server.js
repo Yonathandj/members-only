6ce52e65b9f6525d09f1067b970f935beab3baa4
Yonathan Dawang Jati <yonathandawang111@gmail.com>
2023-10-23 13:28:14 +0700

    Initialize server port and route

--- server/src/server.js
+++ server/src/server.js
@@ -3,12 +3,14 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 
+const signUpRouter = require('./routes/signUp');
 
+app.use('/sign-up', signUpRouter);
 
 app.use((err, req, res, next) => {
     res.status(500).json(err)
 })
 
-app.listen(SERVER_PORT, () => {
-    console.log(`server is running on port:${SERVER_PORT}`)
+app.listen(process.env.SERVER_PORT, () => {
+    console.log(`server is running on port:${process.env.SERVER_PORT}`)
 })
